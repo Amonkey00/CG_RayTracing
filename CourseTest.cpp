561a3f9444a110e262c4da359e454634294e6130
Amonkey00 <892542697@qq.com>
2021-06-27 13:04:43 +0800

    Complete 5 Perlin Texture

--- CourseTest.cpp
+++ CourseTest.cpp
@@ -70,6 +70,16 @@ hittable_list two_spheres() {
     return objects;
 }
 
+hittable_list two_perlin_spheres() {
+    hittable_list objects;
+
+    auto pertext = make_shared<noise_texture>(4);
+    objects.add(make_shared<sphere>(point3(0, -1000, 0), 1000, make_shared<lambertian>(pertext)));
+    objects.add(make_shared<sphere>(point3(0, 2, 0), 2, make_shared<lambertian>(pertext)));
+
+    return objects;
+}
+
 color ray_color(const ray& r,const hittable& world,int depth) {
     hit_record rec;
 
@@ -108,7 +118,7 @@ int main() {
     auto vfov = 40.0;
     auto aperture = 0.0;
 
-    switch (0) {
+    switch (3) {
     case 1:
         world = random_scene();
         lookfrom = point3(13, 2, 3);
@@ -116,10 +126,14 @@ int main() {
         vfov = 20.0;
         aperture = 0.1;
         break;
-
-    default:
     case 2:
         world = two_spheres();
+        lookfrom = point3(13, 2, 3);
+        lookat = point3(0, 0, 0);
+        vfov = 20.0;
+        break;
+    case 3:
+        world = two_perlin_spheres();
         lookfrom = point3(13, 2, 3);
         lookat = point3(0, 0, 0);
         vfov = 20.0;
--- texture.h
+++ texture.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "rtweekend.h"
+#include "perlin.h"
 
 class texture {
 public:
@@ -44,4 +45,17 @@ public:
 public:
 	shared_ptr<texture> odd;
 	shared_ptr<texture> even;
+};
+
+class noise_texture : public texture {
+public:
+	noise_texture():scale(1.0){}
+	noise_texture(double sc):scale(sc){}
+
+	virtual color value(double u, double v, const point3& p)const override {
+		return color(1, 1, 1) * 0.5 * (1 + sin(scale * p.z() + 10 * noise.turb(p)));
+	}
+public:
+	perlin noise;
+	double scale;
 };
